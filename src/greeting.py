a409980ab19172997f465e550f94247ccab3afdc
nc-liam-greenfield <152385929+nc-liam-greenfield@users.noreply.github.com>
2024-08-07 09:59:05 +0100

    make greeting dynamic

--- src/greeting.py
+++ src/greeting.py
@@ -2,4 +2,4 @@ def greet_person(name):
     if name == 'anon':
         return 'shhhh secret greeting'
     # Needs to take name param
-    return 'Good morning Nadia!'
+    return f'Good morning {name}!'
